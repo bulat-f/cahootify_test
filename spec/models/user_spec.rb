f4ff21ec7109646a5ab9cd402745ce4a393727d8
Bulat F <write2bulat@gmail.com>
2017-08-13 16:15:35 +0300

    Add email validation to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    let(:user) { FactoryGirl.build :user, email: email }
+    context 'valid email' do
+      let(:email) { 'valid.email@example.com' }
+      it { expect(user.valid?).to eql true }
+    end
+
+    context 'invalid email' do
+      let(:email) { 'invalid email@example.com' }
+      it { expect(user.valid?).to eql false }
+    end
+  end
 end
